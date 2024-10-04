a48be0bcbd6485f54f3befcb08a73c90699e8d3e
Pranav Titambe <pranavtitambe04@gmail.com>
2024-10-04 21:15:32 +0530

    Added radial gradient background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,27 +3,28 @@ import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Skills from "../components/Skills";
 import LandingPage from "../components/LandingPage";
-import Experience from "./Experience";
-import { Canvas } from "@react-three/fiber";
 import Avatar from "../components/Avatar";
+import "./styles.css"
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen overflow-hidden absolute w-full m-0">
-        <LandingPage />
-        <div className="pt-20 h-screen bg-[#242424]" id="home">
-          <Avatar/>
+      <div className="main">
+        <div className="h-screen overflow-hidden w-full m-0">
+          <LandingPage />
+          <div className="pt-20 h-screen" id="home">
+            <Avatar />
+          </div>
         </div>
-      </div>
-      <div className="pt-20 h-screen bg-lime-200" id="about">
-        <About />
-      </div>
-      <div className="pt-20 h-screen bg-lime-300" id="skills">
-        <Skills />
-      </div>
-      <div className="pt-20 h-screen bg-lime-400" id="contact-me">
-        <ContactMe />
+        <div className="pt-20 h-screen" id="about">
+          <About />
+        </div>
+        <div className="pt-20 h-screen" id="skills">
+          <Skills />
+        </div>
+        <div className="pt-20 h-screen" id="contact-me">
+          <ContactMe />
+        </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "radial-gradient":
+          "radial-gradient(circle, rgba(255,0,150,1), rgba(0,204,255,1))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
