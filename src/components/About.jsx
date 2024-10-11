f80ba33c8cf0b28b3424c8fba264d9fcee7c40bd
Pranav Titambe <pranavtitambe04@gmail.com>
2024-10-11 22:16:08 +0530

    Added About me section to portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function About() {
   return (
     <>
-      <div className='text-white'>About</div>
+      <div></div>
+      <div className="mx-auto px-[100px]">
+        <Card className="bg-[#ffffff24] shadow-2xl border-transparent">
+          <CardHeader>
+            <CardTitle className="text-white text-2xl">About Me</CardTitle>
+            <CardDescription className="text-slate-300">
+              Btw I use Linux 🖥️
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col md:flex-row justify-center items-center text-white">
+            <div className="px-5">
+              <img
+                src="/pranav-photo-2.png"
+                alt="My Photo"
+                className="h-[250px] w-[250px] rounded-full"
+              />
+            </div>
+            <div className="px-5">
+              <p className="text-white">
+                I am a software engineer with a passion for web development.
+              </p>
+              <p className="text-white">
+                I am a self-taught developer and I have been coding for over 2
+                years.
+              </p>
+
+              <p className="text-white">
+                I have worked on various projects and have experience in
+                building full-stack applications.
+              </p>
+
+              <p className="text-white">
+                I am passionate about coding and I enjoy building applications
+                that solve real-world problems.
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,65 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-  	extend: {
-  		backgroundImage: {
-  			'radial-gradient': 'radial-gradient(circle, rgba(255,0,150,1), rgba(0,204,255,1))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		}
-  	}
+  darkMode: ["class"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {	
+    extend: {		
+      backdropFilter: {
+        backdropFilter:
+          "var(--tw-backdrop-blur) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia)",
+      },
+      backgroundImage: {
+        "radial-gradient":
+          "radial-gradient(circle, rgba(255,0,150,1), rgba(0,204,255,1))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
